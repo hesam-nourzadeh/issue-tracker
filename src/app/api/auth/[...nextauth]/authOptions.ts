518d42c159e84d231d6b83d10f78f0d345976a77
Ali Ahmadi <mrkiller1383@gmail.com>
2025-05-02 17:22:45 +0330

    Added github oAuth. Authorizaton applied on /issues/list

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
 import prisma from "../../../../../prisma/client";
 
 export const authOptions: AuthOptions = {
@@ -8,6 +10,10 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
   adapter: PrismaAdapter(prisma),
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/issues/new", "/issues/:id/:path+"],
+  matcher: ["/issues/new", "/issues/:id/:path+", "/issues/list"],
 };
